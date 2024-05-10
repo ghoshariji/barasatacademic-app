c0f78b8e3505f76486e6f2dc464efe9a8f9f82c1
Niladri Sekhar Rout <niladri30012003@gmail.com>
2024-05-10 16:01:04 +0530

    payment

--- app-pay/src/component/Payment.js
+++ app-pay/src/component/Payment.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import "../customcss/payment.css";
+import img from "../images/payment-card.jpeg"
 const Payment = () => {
   const {
     couqidsfuegotoId,
@@ -49,11 +51,47 @@ const Payment = () => {
   useEffect(() => {
     // handlePayment()
   }, []);
+  //http://localhost:3000/gateway-integration-payment/V1/321/fsddfds78vcx/security-allow-allregistion/321/321321/854587877fdsdssdsf/rwhefdfdsf/8844/321132/321321/8999/1233/dshdadghjasg/2732544543eww/321/jdasdjkldsa55545/7473hdhdhsahgh/dsasadaa%22
   return (
-    <div>
-      <h1>Hello</h1>
-      <button onClick={handlePayment}>Pay Now</button>
+
+
+   <div className="main">
+
+
+    <div className="payment-card">
+      <div className="image">
+        <img src={img} alt="" className="payment-pic" />
+      </div>
+
+      <div className="payment-data">
+        <h2>Make Payment</h2>
+        <span>Price is: {price}</span>
+      </div>
+      <div className="row">
+        <div className="info">
+          <h3>Email</h3>
+          <span>{email}</span>
+        </div>
+        <div className="info">
+          <h3>Name</h3>
+          <span>{namegoestothere}</span>
+        </div>
+        <div className="info">
+          <h3>Course</h3>
+          <span>{forgettingjobgoodthis}</span>
+        </div>
+      </div>
+      <div className="pay-btn">
+        <button className="btn100" onClick={handlePayment}>
+          Pay Now
+        </button>
+      </div>
     </div>
+
+    </div>
+    
+
+
   );
 };
 
